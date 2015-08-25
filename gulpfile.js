4e8ea727f271b2059c01a0d68b611ec1bd8965eb
jdungan <johnadungan@gmail.com>
2015-08-24 20:50:46 -0500

    add gulpfile and npm package

---
+++ gulpfile.js
@@ -0,0 +1,22 @@
+var source = require('vinyl-source-stream');
+var gulp = require('gulp');
+var gutil = require('gulp-util');
+var browserify = require('gulp-browserify');
+var watchify = require('watchify');
+var notify = require("gulp-notify");
+
+
+var dirs = {
+  SRC : './src/js',
+  PROD : './pub'
+}
+
+gulp.task('browser', function() {
+    // Single entry point to browserify 
+    gulp.src('src/js/app.js')
+        .pipe(browserify({
+          insertGlobals : true
+        }))
+        .pipe(gulp.dest('./pub/js'))
+});
+
